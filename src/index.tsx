4576ccd237308355f12d792c7e17d250d8104b1b
NiuWeb <niuweb1channel@gmail.com>
2022-05-17 20:20:44 +0000

    promise.then

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-async function index() {
-  await init();
+
+init().then(() => {
   const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
   );
   root.render(<React.StrictMode><App /></React.StrictMode>);
-}
-
-index();
+});
